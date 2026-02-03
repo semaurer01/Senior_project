6d47bf19a900a40c44ce5a193a4cba4fba8b0680
semaurer01 <semaurer01@indianatech.net>
2026-02-03 07:47:13 -0500

    Fixed configuration issues in the setup function allowing the GPS to opperate over QWIIC (WIRE 1) reliably.

--- Arduino/GPS_test/GPS_test.ino
+++ Arduino/GPS_test/GPS_test.ino
@@ -3,7 +3,7 @@
 
 I2CGPS myI2CGPS; // Hook object to the library
 TinyGPSPlus gps;
-
+time_t last_update=0;
 void setup() {
   Serial.begin(115200);
   Serial.println("XA1110 Robust I2C GPS Example");
@@ -13,20 +13,24 @@ void setup() {
     Serial.println("Module failed to respond. Check wiring!");
     delay(500);
   }
+  Wire1.setClock(400000); // 400 kHz
+
   Serial.println("GPS module found!");
+    // Enable debugging output
+  myI2CGPS.enableDebugging(Serial);
+    // Build the PMTK packet for 10 Hz (100 ms)
+  String config = myI2CGPS.createMTKpacket(220, ",100"); 
+  // Send it
+  myI2CGPS.sendMTKpacket(config);
 }
 
 void loop() {
   // Read all available bytes from GPS
-  while (myI2CGPS.available()) {
-    char incoming = (char)myI2CGPS.read(); // Cast to char
+    int bytesAvailable = myI2CGPS.available();
 
-    // Filter non-printable bytes
-      Serial.print((int)incoming);
-      Serial.print(" ");
-
-    // Feed TinyGPSPlus parser
-    gps.encode(incoming);
+  for (int i = 0; i < bytesAvailable; i++) {
+    char c = myI2CGPS.read();
+    gps.encode(c);
   }
 
   // Only print updated location when new data is available
@@ -43,5 +47,10 @@ void loop() {
     Serial.print(gps.hdop.hdop());
 
     Serial.println();
+    last_update=millis();
+  }else if(millis()>last_update+1000){
+    Serial.print("no data Sats: ");
+    Serial.println(gps.satellites.value());
+    last_update=millis();
   }
 }
