7247cd81f6d54132d5dbda0dfb850255b8378028
semaurer01 <semaurer01@indianatech.net>
2026-02-25 12:03:56 -0500

    Added throttle and steering so now the Machine can drive in a strait line.

--- Arduino/Senior_project/Senior_project.ino
+++ Arduino/Senior_project/Senior_project.ino
@@ -2,20 +2,24 @@
 #define max_steering_angle 100 //maximum angle in degrees
 #include <Servo.h>
 
-Servo steering;  // create servo object to control a servo
+Servo steering;  // create servo object to control steering. this is the front servo
+Servo throttle;  //create another servo object to control throttle this actually controls the esc
 // twelve servo objects can be created on most boards
 
 int steering_angle = 90;    // variable to store the servo position
+int throttle_speed = 87;  //90 is stopped lower numbers faster. currently set very slow for testing
 
 void setup() {
-  steering.attach(D4);  // attaches the servo on pin 4 to the servo object
+  steering.attach(D4);  // attaches the servo on pin 4 to the steering object
+  throttle.attach(D8); //attaches the ESC on pin 8 to the throttle object
 }
 
 void drive(){//this function controls the main driving opperations which must be done repeatedly.
-  steering.write(steering_angle);
+  steering.write(steering_angle);//set the steering angle
+  Throttle.write(throttle_speed);//set the speed
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  drive();
 }
