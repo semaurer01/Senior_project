d9f0c6b6c245f3688bde6e8bb43b22b002ed7994
semaurer01 <semaurer01@indianatech.net>
2026-03-31 10:59:46 -0400

    minor changes to apearence of code to look more like my other code

--- Arduino/double_speedometer_test/double_speedometer_test.ino
+++ Arduino/double_speedometer_test/double_speedometer_test.ino
@@ -4,19 +4,19 @@ volatile unsigned long lastPulseLeft = 0;
 volatile unsigned long lastPulseRight = 0;
 volatile float speedLeft = 0;
 volatile float speedRight = 0;
-//meters per speedometer pulse at 2 pulses per revolution this is π*r where r is the radius of the wheel
-float distancePerPulse = PI*.07;
+//meters per speedometer pulse at 2 pulses this is π*r where r is the radius of the wheel
+#define distancePerPulse = (PI * 0.03);
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(2, INPUT); // left wheel
-  pinMode(3, INPUT); // right wheel
-
-  attachInterrupt(digitalPinToInterrupt(2), leftWheelISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), rightWheelISR, RISING);
+  pinMode(D2, INPUT); // left wheel
+  pinMode(D3, INPUT); // right wheel
+  //placing the left wheel on 2 and the right one on 3 prevents these wires from crossing
+  attachInterrupt(digitalPinToInterrupt(D2), leftWheelISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(D3), rightWheelISR, RISING);
 }
-
+//ISRs for speedometer are separate in order to save time on execution
 // ISR for left wheel
 void leftWheelISR() {
   unsigned long now = micros();
@@ -57,5 +57,5 @@ void loop() {
   Serial.print(", Left Speed: "); Serial.print(leftSpeed);
   Serial.print(" m/s, Right Speed: "); Serial.println(rightSpeed);
 
-  delay(100); // update display
+  delay(100); // update display delays are safe here because the interupts are still running
 }
